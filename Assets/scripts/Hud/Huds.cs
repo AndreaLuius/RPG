c35af75db3771f78418ac41ab3c9abaebed85c9a
AndreaLuius <andrea.luiu@libero.it>
2020-04-23 00:36:24 +0200

    pickup v2,fixed some bugs of 2nd version of pickupweapon system

---
+++ Assets/scripts/Hud/Huds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Hud
+{
+    public class Huds : MonoBehaviour
+    {
+        [SerializeField] GameObject panel;
+
+
+        public void closePanel()
+        {
+            panel.SetActive(false);
+        }
+
+        public void openPanel()
+        {
+            panel.SetActive(true);
+        }
+    }
+
+}
--- Assets/scripts/combat/WeaponPickup.cs
+++ Assets/scripts/combat/WeaponPickup.cs
@@ -1,4 +1,4 @@
-using System;
+using RPG.Hud;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -6,15 +6,40 @@ namespace RPG.Combat
     public class WeaponPickup: MonoBehaviour
     {
         [SerializeField] private Weapon weapon;
-
+        [SerializeField] private Huds hud;
+        private Fighter fighter = null;
+        public bool isCollectible = false;
+        
+        private void Update()
+        {
+            equipEnabling();
+        }
+        
+        private void equipEnabling()
+        {
+            if (isCollectible && Input.GetKeyDown(KeyCode.F))
+            {
+                fighter.equipWeapon(weapon);
+                hud.closePanel();
+                Destroy(gameObject);
+            }
+        }
+        
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag.Equals("Player"))
             {
-                print("here");
-                other.GetComponent<Fighter>().equipWeapon(weapon);
-                Destroy(gameObject);
+                hud.openPanel();
+                isCollectible = true;
+                fighter = other.GetComponent<Fighter>();
             }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            hud.closePanel();
+            isCollectible = false;
+            fighter = null;
         }
     }
 }
